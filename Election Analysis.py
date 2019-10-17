51962af6fe10ea3adae5ac87f55aa454dce55306
jaychung0809 <55941125+jaychung0809@users.noreply.github.com>
2019-10-16 19:50:35 -0700

    Rename

---
+++ Election Analysis.py
@@ -0,0 +1,5 @@
+#Open the data file.
+#Write down the names of all the candidates.
+#Add a vote count for each candidate.
+#Get the total votes for each candidate.
+#Get the total votes cast for the election.
