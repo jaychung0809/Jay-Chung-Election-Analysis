167556e579eebfcaa6ba692d05add65830b8d6a7
jaychung0809 <55941125+jaychung0809@users.noreply.github.com>
2019-10-18 01:47:06 -0700

    update
    Update

--- Election Analysis.py
+++
@@ -1,5 +0,0 @@
-#Open the data file.
-#Write down the names of all the candidates.
-#Add a vote count for each candidate.
-#Get the total votes for each candidate.
-#Get the total votes cast for the election.
